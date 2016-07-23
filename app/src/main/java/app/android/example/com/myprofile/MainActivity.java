bedc3d699efc45a9ba803d3378fc476164811f71
Sujeeth Jinesh <sujeethjinesh@gmail.com>
2016-07-23 12:28:56 -0700

    third commit

--- app/src/main/java/app/android/example/com/myprofile/MainActivity.java
+++ app/src/main/java/app/android/example/com/myprofile/MainActivity.java
@@ -1,7 +1,11 @@
 package app.android.example.com.myprofile;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +14,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void emailMessage(View view) {
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:"));
+
+        EditText editText = (EditText) findViewById(R.id.my_edit_text);
+
+        intent.putExtra(Intent.EXTRA_EMAIL, new String[]{"sujeethjinesh@gmail.com"});
+        intent.putExtra(Intent.EXTRA_TEXT, editText.getText());
+
+        if(intent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(intent, 0);
+        }
+    }
+
 }
